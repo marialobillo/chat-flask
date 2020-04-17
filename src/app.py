d7f4195ac27a3d5ff206092b2be8967db67909e7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-04-17 13:30:25 +0200

    run the app

---
+++ src/app.py
@@ -0,0 +1,17 @@
+from flask import Flask 
+from .config import app_config
+
+def create_app(env_name):
+    """
+    Create app
+    """
+
+    app = Flask(__name__)
+
+    app.config.from_object(app_config[env_name])
+
+    @app.route('/', methods=['GET'])
+    def index():
+        return "Here is the root"
+
+    return app
