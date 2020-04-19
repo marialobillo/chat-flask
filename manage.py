04d3ad5ce24d2709f4eb165a7a410655b41581ca
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-04-19 18:49:31 +0200

    fixing errors

--- manage.py
+++ manage.py
@@ -1,8 +1,9 @@
-import os 
-from flask_script import Manager 
-from flask_migrate import Migrate, MigrateCommand 
+import os
+from flask_script import Manager
+from flask_migrate import Migrate, MigrateCommand
 
-from src.app import create_app, db 
+from src.app import create_app, db
+
 
 env_name = os.getenv('FLASK_ENV')
 app = create_app(env_name)
@@ -14,4 +15,4 @@ manager = Manager(app=app)
 manager.add_command('db', MigrateCommand)
 
 if __name__ == '__main__':
-    manager.run()
+  manager.run()
--- run.py
+++ run.py
@@ -1,9 +1,14 @@
-import os 
+import os
+from dotenv import load_dotenv, find_dotenv
 
-from src.app import create_app 
+from src.app import create_app
+
+load_dotenv(find_dotenv())
+
+env_name = os.getenv('FLASK_ENV')
+app = create_app(env_name)
 
 if __name__ == '__main__':
-    env_name = os.getenv('FLASK_ENV')
-    app = create_app(env_name)
-
-    app.run()
+  port = os.getenv('PORT')
+  # run app
+  app.run(host='0.0.0.0', port=port)
