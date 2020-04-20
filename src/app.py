50874394e313d0c1a1b196b19be27942442c01f1
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-04-20 17:35:52 +0200

    channel model class

--- src/app.py
+++ src/app.py
@@ -49,6 +49,42 @@ class UserModel(db.Model):
         return UserModel.query.get(id)
 
 
+class ChannelModel(db.Model):
+    __tablename__ = 'channels'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(128))
+    description = db.Column(db.String(255))
+    created_at = db.Column(db.DateTime)
+    modified_at = db.Column(db.DateTime)
+
+    def __init__(self, data):
+        self.name = data.get('name')
+        self.description = data.get('description')
+        self.created_at = datetime.datetime.utcnow()
+        self.modified_at = datetime.datetime.utcnow()
+
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def update(self, data):
+        for key, item in data.items():
+            setattr(self, key, item)
+        self.modified_at = datetime.datetime.utcnow() 
+        db.session.commit() 
+
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit() 
+
+    @staticmethod
+    def get_all_channels():
+        return ChannelModel.query.all()
+
+    @staticmethod
+    def get_one_channel(id):
+        return ChannelModel.query.get(id)
 
 @app.route('/')
 def hello():
