6f854f728d527ad7aef2a68df02f8644744589e7
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-04-22 12:11:33 +0200

    endpoints for user

--- app.py
+++ app.py
@@ -9,7 +9,6 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 from models import User, Channel, Message
-from routes import routes 
 
 
 @app.route("/")
@@ -17,6 +16,62 @@ def hello():
     return "Hello Root"
 
 
+@app.route('/users', methods=['POST', 'GET'])
+def add_user():
+    if request.method == 'POST':
+        if request.is_json:
+            data = request.get_json()
+            new_user = User(
+                username = data['username'],
+                email = data['email'],
+                password = data['password']
+            )
+            db.session.add(new_user)
+            db.session.commit()
+            return {"message": f"User {new_user.username} has been created successfully."}
+        else:
+            return {"error": "The request payload is not is JSON format"}
+
+    elif request.method == 'GET':
+        users = User.query.all()
+        results = [
+            {
+                "id": user.id,
+                "username": user.username,
+                "email": user.email,
+                "password": user.password,
+                "created_at": user.created_at
+            } for user in users
+        ]
+        return {"count": len(results), "users": results}
+
+
+
+@app.route('/users/<user_id>', methods=['GET', 'PUT', 'DELETE'])
+def handle_user(user_id):
+    user = User.query.get_or_404(user_id)
+
+    if request.method == 'GET':
+        response = {
+            "username": user.username,
+            "email": user.email,
+            "password": user.password
+        }
+        return {"message" : "success", "user": response}
+
+    elif request.method == 'PUT':
+        data = request.get_json()
+        user.username = data['username']
+        user.email = data['email']
+        user.password = data['password']
+        db.session.add(user)
+        db.session.commit()
+        return { "message": f"User {user.username} successfully updated." }
+
+    elif request.method == 'DELETE':
+        db.session.delete(user)
+        db.session.commit()
+        return {"message": f"User {user.username} successfully deleted."}
 
 if __name__ == '__main__':
     app.run()
