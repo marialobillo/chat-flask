283b5cc63b22c0b7dfd2a8029bb8b15db5168bb6
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-11 12:50:46 +0200

    tables for database: done

--- app/__init__.py
+++ app/__init__.py
@@ -1,10 +1,18 @@
 from flask import Flask
 
 from .models import db
+from .models.user import User 
+from .models.channel import Channel
+from .models.message import Message
 
 app = Flask(__name__)
 
 def create_app(environment):
+
     app.config.from_object(environment)
 
+    with app.app_context():
+        db.init_app(app)
+        db.create_all()
+    
     return app
