3b22df74f2ae16bc84d1eaef9d23b23f50cfe599
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-11 11:19:22 +0200

    config for database

--- config.py
+++ config.py
@@ -6,7 +6,8 @@ class Config:
 
 class DevelopmentConfig(Config):
     DEBUG = True 
-
+    SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root123@localhost:5432/chat_dev'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
 config = {
     'development': DevelopmentConfig
