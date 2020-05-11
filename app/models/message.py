7e2cca32211b93e68f5955888b6711e2706b9dd4
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-11 12:26:48 +0200

    implementing models for user and message

--- app/models/message.py
+++ app/models/message.py
@@ -1,4 +1,16 @@
 from . import db  
 
 class Message(db.Model):
-    pass
+    
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), 
+        nullable=False)
+    channel_id = db.Column(db.Integer, db.ForeignKey('channel.id'),
+        nullable=False)
+    content = db.Column(db.Text(), nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False, 
+                default=db.func.current_timestamp())
+
+
+    def __str__(self):
+        return self.content
--- app/models/user.py
+++ app/models/user.py
@@ -1,4 +1,13 @@
 from . import db 
 
 class User(db.Model):
-    pass 
+
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    created_at = db.Column(db.DateTime(), nullable=False, 
+                default=db.func.current_timestamp())
+
+
+    def __str__(self):
+        return self.username
