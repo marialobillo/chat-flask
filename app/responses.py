16707b35ea18aa8c4d290f36c4af0b6fcb3d5bd8
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-15 12:13:08 +0200

    validation function, not_found

--- app/responses.py
+++ app/responses.py
@@ -9,3 +9,13 @@ def response(data):
         }
     ), 200
 
+def not_found():
+    return jsonify(
+        {
+            'success': False, 
+            'data': {},
+            'message': 'Resource not found',
+            'code': 404
+        }
+    ), 404
+
