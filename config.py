6156ee8961853c92510ab3fd4f4dc926cd5ccbfa
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-16 13:28:50 +0200

    adding some config for testing

--- config.py
+++ config.py
@@ -9,6 +9,12 @@ class DevelopmentConfig(Config):
     SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:root123@localhost:5432/chat_dev'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
 
+class TestConfig(Config):
+    DEBUG = False
+    SQLALCHEMY_DATABASE_URI = 'postgrsql://postgres:root123@localhost:5432/chat_test'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 config = {
-    'development': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'test': TestConfig
 }
--- test.py
+++ test.py
@@ -1,8 +1,13 @@
 import unittest
 
+from config import config
+from app import create_app 
+
+
 class TestAPI(unittest.TestCase):
     def setUp(self):
-        pass 
+        environment = config['test']
+        self.app = create_app(environment) 
 
     def tearDown(self):
         pass
