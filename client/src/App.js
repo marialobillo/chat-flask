98110c97b6477c0f4fcc61a684a1cbcf63748f0f
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-05-31 12:46:18 +0200

    login routes

--- client/src/App.js
+++ client/src/App.js
@@ -6,12 +6,13 @@ import Axios from 'axios';
 import Login from './components/Login';
 import Register from './components/Register';
 import Chat from './components/Chat';
+// import Error from './components/Error';
 
 
 export default function App() {
 
   const [user, setUser] = useState(null);
-  const [error, setError] = useState(null);
+  const [error, setError] = useState('no pude cargar feed');
 
   const login = async (username, password) => {
     const url = 'http://localhost:5000/api/login';
@@ -32,9 +33,15 @@ export default function App() {
     setUser(null);
   }
 
+  const showError = (message) => {
+    setError(message);
+  }
+
 
   return (
     <Router>
+
+ 
       { user ? 
         (<LoginRoutes />) 
       : 
@@ -51,7 +58,7 @@ function LoginRoutes(){
     <Switch>
       <Route 
         path='/'
-        component={() => <div><h1>Soy el feed</h1></div>}
+        render={(props) => <Chat />}
         default
         />        
     </Switch>
