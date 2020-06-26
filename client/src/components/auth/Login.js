071ce692d81f7fdfd21497a2b28ce9f69680b3a5
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-06-26 20:22:20 +0200

    login component

--- client/src/components/auth/Login.js
+++ client/src/components/auth/Login.js
@@ -1,9 +1,76 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Login = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        password: ''
+    });
+
+    const {username, password} = user;
+
+    const onChange = event => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+        // Validation
+
+        // To action
+    }
     return (
-        <div>From Login</div>
+        <div className="container text-center">
+            <div className="login-panel">
+                <h1>Login</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="form-group">
+                        <label htmlFor="email">Username</label>
+                        <input 
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Your Username..."
+                            onChange={onChange}
+                            className="form-control"
+                            value={username}
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your Password..."
+                            onChange={onChange}
+                            className="form-control"
+                            value={password}
+                        />
+                    </div>
+
+                    <div className="from-group">
+                        <input 
+                            type="submit" 
+                            className="btn btn-block btn-success"
+                            value="Login"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/register'} className="">
+                    Get a New Account
+                </Link>
+            </div>
+        </div>
     );
 }
 
