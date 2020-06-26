8b4b65acbd3286d2d150e3f2be316b3c9f45c4d5
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-06-26 20:22:28 +0200

    register component

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,9 +1,95 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const [user, setUser] = useState({
+        username: '',
+        password: '',
+        confirm: ''
+    });
+
+    const { username, password, confirm} = user;
+
+    const onChange = event => {
+        setUser({
+            ...user,
+            [event.target.name]: event.target.value
+        })
+    }
+
+    const onSubmit = event => {
+        event.preventDefault();
+
+        // Validation
+
+        // password min 6 char
+
+        // cofirm and password equal
+
+        // To action
+    }
     return (
-        <div>From Register</div>
+        <div className="container text-center">
+            <div className="login-panel">
+                <h1>Register</h1>
+
+                <form
+                    onSubmit={onSubmit}
+                >
+                    <div className="form-group">
+                        <label htmlFor="username">Username</label>
+                        <input 
+                            type="text"
+                            id="username"
+                            name="username"
+                            placeholder="Your Username..."
+                            onChange={onChange}
+                            className="form-control"
+                            value={username}
+                        />
+                    </div>
+                    
+                    <div className="form-group">
+                        <label hrmlFor="password">Password</label>
+                        <input 
+                            type="password"
+                            id="password"
+                            name="password"
+                            placeholder="Your Password..."
+                            onChange={onChange}
+                            className="form-control"
+                            value={password}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label hrmlFor="confirm">Confirm Password</label>
+                        <input 
+                            type="password"
+                            id="confirm"
+                            name="confirm"
+                            placeholder="Please, repeat your Password..."
+                            onChange={onChange}
+                            className="form-control"
+                            value={confirm}
+                        />
+                    </div>
+
+                    <div className="from-group">
+                        <input 
+                            type="submit" 
+                            className="btn btn-block btn-success"
+                            value="Register"
+                        />
+                    </div>
+                </form>
+
+                <Link to={'/'} className="">
+                    Login
+                </Link>
+            </div>
+        </div>
     );
 }
 
