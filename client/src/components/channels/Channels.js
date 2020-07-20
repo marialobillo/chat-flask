3fccee789f0ab2c9904dcff48d6f11ef41636f3a
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-07-20 13:46:01 +0200

    channels

--- client/src/components/channels/Channels.js
+++ client/src/components/channels/Channels.js
@@ -1,11 +1,21 @@
-import React from 'react';
+import React, { useContext, useEffect } from 'react';
 import Navbar from '../layout/Navbar';
 import Sidebar from '../layout/Sidebar';
 import FormMessage from '../messages/FormMessage';
 import MessageList from '../messages/MessageList';
 
+import AuthContext from '../../context/authentication/authContext';
 
 const Channels = () => {
+
+    // User
+    const authContext = useContext(AuthContext);
+    const { getAuthenticatedUser } = authContext;
+
+    useEffect( () => {
+        getAuthenticatedUser();
+    }, []);
+
     return (
         <div className="">
             <Navbar />
