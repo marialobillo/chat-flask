e6ee54c27bb0e57a477ba853f12b55be34405216
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-08-03 18:07:52 +0200

    higher order component , privateroute

---
+++ client/src/components/routes/PrivateRoute.js
@@ -0,0 +1,21 @@
+import React, { useContext, useEffect } from 'react';
+import { Route, Redirect } from 'react-router-dom';
+import AuthContext from '../../context/authentication/authContext';
+
+
+const PrivateRoute = ({ component: Component, ...props }) => {
+
+    const authContext = useContext(AuthContext);
+    const { authenticated } = authContext;
+
+    return (
+        <Route { ...props } render={ props => !authenticated ? (
+            <Redirect to="/" />
+        ) : (
+            <Component {...props} />
+        ) }
+        />
+     );
+}
+
+export default PrivateRoute;
