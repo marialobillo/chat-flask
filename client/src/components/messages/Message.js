e1748ae89b1f59360f564beadc72167ea8388364
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-08-08 18:21:43 +0200

    css for messages

--- client/src/components/messages/Message.js
+++ client/src/components/messages/Message.js
@@ -7,8 +7,15 @@ const Message = ({message}) => {
     return (
         <li key={message.id}>
            <div className="message">
-                <div className="message-date">{moment(message.created_at).fromNow()}</div>
-                <div className="message-username"> <strong>{message.username}: </strong></div>
+                <div className="row">
+                <span className="message-username"> 
+                    <strong>{message.username} </strong></span>
+                <span className="message-date">
+                    {moment(message.created_at).fromNow()}
+                </span>
+
+                </div>
+               
                 <span>{message.content}</span>
                 
             </div>
