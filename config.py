e6ca3712c6799da3312f5884dbb33b6dd684d3cc
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2020-08-15 12:07:46 +0200

    production config

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ class Config:
 
 
 class ProductionConfig(Config):
-    DEBUG = True 
-    SQLALCHEMY_DATABASE_URI = ''
+    DEBUG = False 
+    SQLALCHEMY_DATABASE_URI = 'postgres://jbidrpgimxwynr:4a2d7ec8d8516f7cc17c1166bd4dd38bc68ab99ae0bdfeacc2a446092be39894@ec2-34-236-215-156.compute-1.amazonaws.com:5432/dah5cftofl4bte'
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
     SECRET_WORD = 'theredcatisblue'
 
--- main.py
+++ main.py
@@ -2,7 +2,8 @@ from app import create_app
 from flask_cors import CORS
 from config import config
 
-environment = config['development']
+# environment = config['development']
+environment = config['production']
 
 app = create_app(environment)
 cors = CORS(app) 
